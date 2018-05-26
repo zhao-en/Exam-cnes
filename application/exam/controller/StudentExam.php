d2eab31af64ab096f5876b0aecfec50c6ae63349
en_zhao <2399144252@qq.com>
2018-05-26 23:46:24 +0800

    #save 修改

---
+++ application/exam/controller/StudentExam.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: en_zhao
+ * Date: 2018/5/26
+ * Time: 23:42
+ */
+
+namespace app\exam\controller;
+
+
+
+use think\Controller;
+
+class StudentExam extends Controller
+{
+
+}
